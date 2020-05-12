c24581626d786d65ac0e4c8905605c3b418f139b
unix <unix.bio@gmail.com>
2020-05-12 14:38:59 +0800

    chore: upgrade next.js

--- next.config.js
+++ next.config.js
@@ -7,25 +7,25 @@ const withMDX = require('@next/mdx')({
 
 const nextConfig = {
   target: 'serverless',
-  
+
   pageExtensions: ['jsx', 'js', 'mdx', 'md', 'ts', 'tsx'],
-  
+
   cssModules: true,
-  
+
   cssLoaderOptions: {
     importLoaders: 1,
     localIdentName: '[local]___[hash:base64:5]',
   },
-  
+
   env: {
     VERSION: require('./package.json').version,
   },
-  
+
   webpack(config) {
     config.resolve.modules.push(__dirname)
     return config
   },
-  
+
   experimental: {
     reactRefresh: true,
   },
